5398a6e712d001dbbbd5e7b9a39610163d91442e
Matias GIl <matiasgil@igapps-server-2.local>
2014-08-27 16:20:38 -0500

    Initial Commit

---
+++ minPlus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  minPlus
+//
+//  Created by Matias GIl on 8/27/14.
+//  Copyright (c) 2014 Matias GIl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
